479dc683d9e431cdea2e398847296a25ff530753
Vu Hoang <tienvu95@gmail.com>
2023-12-27 00:22:28 +0700

    add req

--- calculator.py
+++ calculator.py
@@ -1,14 +1,11 @@
 """
-Unit tests for the calculator library
+Calculator library containing basic math operations.
 """
 
-import calculator
+
+def add(first_term, second_term):
+    return first_term + second_term
 
 
-class TestCalculator:
-
-    def test_addition(self):
-        assert 4 == calculator.add(2, 2)
-
-    def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+def subtract(first_term, second_term):
+    return first_term - second_term
